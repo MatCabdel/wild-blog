b39768f3c8afa6f1c4ebe033b77dccc6f0291359
Mathieu Chauveau <chauveau.math@gmail.com>
2024-09-25 16:42:23 +0200

    quest binding ok

--- src/app/article-component/article-component.component.ts
+++ src/app/article-component/article-component.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-article-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './article-component.component.html',
-  styleUrl: './article-component.component.scss'
+  styleUrl: './article-component.component.scss',
 })
 export class ArticleComponentComponent {
+  article = {
+    title: 'Mon premier article',
+    author: 'Moi même, le 25 septembre 2025',
+    content:
+      'Alors dans mon article ça va surtout parler de perruches et de Francois Damiens',
+    image: 'https://via.placeholder.com/350x150',
+    isPublished: true,
+    comment: '',
+  };
 
+  togglePublication(): void {
+    this.article.isPublished = !this.article.isPublished;
+  }
 }
